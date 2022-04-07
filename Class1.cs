3e155b2270fdf903c1eda9412f0f08abcfbafb4e
PC <PC@DESKTOP-LUEVN40>
2022-04-08 00:52:41 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+internal  class Rettangolo
+{
+        private int baseRettangolo;
+
+        private int altezzaRettangolo;
+    
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+{
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+}
+public void calcolaArea()
+        {
+            Console.WriteLine("L'area è");
+            calcolaArea = (baseRettangolo * altezzaRettangolo);
+        }
+
+        public void calcolaPerimetro()
+        {
+            Console.WriteLine("Il perimetro è");
+            calcolaPerimetro = baseRettangolo + altezzaRettangolo;
+        }
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("Il perimetro è" + calcolaPerimetro);
+            Console.WriteLine("L'area è" + calcolaArea);
+        }
+    }
+}
---
+++ Geometria/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Inserisci base");
+int baseRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci altezza");
+int altezzaRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine("------Ecco l'area del rettangolo -------:  " +baseRettangolo*altezzaRettangolo + "cm^2");
+Console.WriteLine("------Ecco il perimetro del rettangolo-------:  " + (baseRettangolo + altezzaRettangolo)*2 + "cm");
